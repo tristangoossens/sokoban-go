dc39d4b74f9e675d5d367ac5ca76c24742178235
Tristan Goossens <tristan.goossens@idtrust.com>
2019-11-12 16:03:39 +0100

    added maplevel into entity.go

--- game/entity.go
+++ game/entity.go
@@ -1,7 +1,12 @@
 package trisoban
 
 import (
+	"bufio"
+	"fmt"
 	tl "github.com/JoelOtter/termloop"
+	"log"
+	"os"
+	"strings"
 )
 
 func NewCrate() *Crate {
@@ -30,4 +35,49 @@ func NewGoal() *Goal {
 	goal.Entity = tl.NewEntity(1, 1, 1, 1)
 
 	return goal
+}
+
+func MapLevel() {
+	var startx = 7 // Determines at which coordinate the rendering starts
+	var starty = 5 // Determines at which coordinate the rendering starts
+
+	border.bCoords = make(map[Coordinates]int)
+
+	file, err := os.Open(fmt.Sprintf("util/levels/level%d.txt", CurrentLevel))
+	if err != nil {
+		log.Fatalln(file)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		xline := strings.Split(scanner.Text(), "")
+		for i, v := range xline {
+			switch v {
+			case "#":
+				border.bCoords[Coordinates{
+					X: startx + i,
+					Y: starty,
+				}] = 1
+			case "G":
+				goal.Coordinates = Coordinates{
+					X: startx + i,
+					Y: starty,
+				}
+			case "P":
+				player.Coordinates = Coordinates{
+					X: startx + i,
+					Y: starty,
+				}
+				player.SetPosition(player.X, player.Y)
+			case "C":
+				crate.Coordinates = Coordinates{
+					X: startx + i,
+					Y: starty,
+				}
+				crate.SetPosition(crate.X, crate.Y)
+			}
+		}
+		starty++
+	}
+	file.Close()
 }
