8c1d3bea7ebca9f4821d99cffe9010a628c89ce8
Tristan Goossens <d267316@edu.rocwb.nl>
2020-05-21 11:23:34 +0200

    implemented multiple crate and goal support

--- game/keyinput.go
+++ game/keyinput.go
@@ -7,11 +7,6 @@ import (
 	tl "github.com/JoelOtter/termloop"
 )
 
-var oldX int
-var oldY int
-var oldcrateX int
-var oldcrateY int
-
 // Tick listens to keyinput on the titlescreen, so if the enter key is pressed, the game will start.
 func (ts *Titlescreen) Tick(event tl.Event) {
 	if event.Type == tl.EventKey {
@@ -29,22 +24,18 @@ func (ts *Titlescreen) Tick(event tl.Event) {
 	}
 }
 
-// Tick listens to the player input and handle them accordingly.
-func (player *Player) Tick(event tl.Event) {
+// Tick listens to the gamescreen input and handles it accordingly.
+func (g *Gamescreen) Tick(event tl.Event) {
 	if event.Type == tl.EventKey {
-		player.X, player.Y = player.Position()
-		oldX, oldY = player.Position()
-		crate.X, crate.Y = crate.Position()
-		oldcrateX, oldcrateY = crate.Position()
 		switch event.Key {
 		case tl.KeyArrowUp:
-			player.CheckCollisions("UP")
+			g.Move("up")
 		case tl.KeyArrowDown:
-			player.CheckCollisions("DOWN")
+			g.Move("down")
 		case tl.KeyArrowLeft:
-			player.CheckCollisions("LEFT")
+			g.Move("left")
 		case tl.KeyArrowRight:
-			player.CheckCollisions("RIGHT")
+			g.Move("right")
 		case tl.KeyF3:
 			RestartLevel()
 		case tl.KeyF1:
@@ -61,104 +52,42 @@ func (player *Player) Tick(event tl.Event) {
 	}
 }
 
-// CheckCollisions will calculate collisions and when there is a collision handle them accordingly.
-//
-// Check 1: Check if the player is colliding with a crate.
-//
-// Check 2: Check if the player is colliding with the border.
-//
-// Check 3: Check if the crate is colliding with the border.
-//
-// Check 4: Check if the crate is colliding with the goal.
-func (player *Player) CheckCollisions(direction string) {
-	switch direction {
-	case "UP":
-		if player.CheckCrateCollision(oldX, oldY-1) {
-			if crate.CheckGoalCollision(oldcrateX, oldcrateY-1) {
-				crate.Y = crate.Y - 1
-				crate.SetPosition(crate.X, crate.Y)
-				LevelCompleted()
+// Move calculate and check collisions for the current move
+func (g *Gamescreen) Move(dir string) {
+	for ci, cv := range col.Crates {
+		if col.Player.CheckCrateCollision(cv, dir) {
+			col.Player.SetPosition(col.Player.X, col.Player.Y)
+			cv.MoveCrate(dir, false)
+			crateslicecopy := cv.CopyCrateSlice()
+			if cv.CheckBorderCollision() {
+				col.Player.MovePlayer(dir, true)
+				cv.MoveCrate(dir, true)
 			}
-			if crate.CheckBorderCollision(oldcrateX, oldcrateY-1) {
-				player.Y = player.Y + 1
-				player.SetPosition(player.X, player.Y)
-				crate.SetPosition(crate.X, crate.Y)
-			} else {
-				crate.Y = crate.Y - 1
-				crate.SetPosition(crate.X, crate.Y)
+			if cv.CheckCrateCollision(cv.RemoveCrate(crateslicecopy, ci)) {
+				cv.MoveCrate(dir, true)
+				col.Player.MovePlayer(dir, true)
 			}
-		}
-		if player.CheckBorderCollision(oldX, oldY-1) {
-			player.SetPosition(player.X, player.Y)
-		} else {
-			player.Y = player.Y - 1
-			player.SetPosition(player.X, player.Y)
-		}
-	case "DOWN":
-		if player.CheckCrateCollision(oldX, oldY+1) {
-			if crate.CheckGoalCollision(oldcrateX, oldcrateY+1) {
-				crate.Y = crate.Y + 1
-				crate.SetPosition(crate.X, crate.Y)
-				LevelCompleted()
+			for _, gv := range col.Goals {
+				if cv.CheckGoalCollision(gv) {
+					if cv.CheckActivatedGoalCollision(gv) {
+						col.Player.MovePlayer(dir, true)
+						cv.MoveCrate(dir, true)
+					} else {
+						gv.isActivated = true
+						gs.RemoveEntity(cv)
+						col.Crates = cv.RemoveCrate(col.Crates, ci)
+					}
+				}
 			}
-			if crate.CheckBorderCollision(oldcrateX, oldcrateY+1) {
-				player.Y = player.Y - 1
-				player.SetPosition(player.X, player.Y)
-				crate.SetPosition(crate.X, crate.Y)
-			} else {
-				crate.Y = crate.Y + 1
-				crate.SetPosition(crate.X, crate.Y)
-			}
-		}
-		if player.CheckBorderCollision(oldX, oldY+1) {
-			player.SetPosition(player.X, player.Y)
-		} else {
-			player.Y = player.Y + 1
-			player.SetPosition(player.X, player.Y)
-		}
-	case "LEFT":
-		if player.CheckCrateCollision(oldX-1, oldY) {
-			if crate.CheckGoalCollision(oldcrateX-1, oldcrateY) {
-				crate.X = crate.X - 1
-				crate.SetPosition(crate.X, crate.Y)
-				LevelCompleted()
-			}
-			if crate.CheckBorderCollision(oldcrateX-1, oldcrateY) {
-				player.X = player.X + 1
-				player.SetPosition(player.X, player.Y)
-				crate.SetPosition(crate.X, crate.Y)
-			} else {
-				crate.X = crate.X - 1
-				crate.SetPosition(crate.X, crate.Y)
-			}
-		}
-		if player.CheckBorderCollision(oldX-1, oldY) {
-			player.SetPosition(player.X, player.Y)
-		} else {
-			player.X = player.X - 1
-			player.SetPosition(player.X, player.Y)
-		}
-	case "RIGHT":
-		if player.CheckCrateCollision(oldX+1, oldY) {
-			if crate.CheckGoalCollision(oldcrateX+1, oldcrateY) {
-				crate.X = crate.X + 1
-				crate.SetPosition(crate.X, crate.Y)
-				LevelCompleted()
-			}
-			if crate.CheckBorderCollision(oldcrateX+1, oldcrateY) {
-				player.X = player.X - 1
-				player.SetPosition(player.X, player.Y)
-				crate.SetPosition(crate.X, crate.Y)
-			} else {
-				crate.X = crate.X + 1
-				crate.SetPosition(crate.X, crate.Y)
-			}
-		}
-		if player.CheckBorderCollision(oldX+1, oldY) {
-			player.SetPosition(player.X, player.Y)
-		} else {
-			player.X = player.X + 1
-			player.SetPosition(player.X, player.Y)
 		}
 	}
+	if col.Player.CheckBorderCollision(dir) {
+		col.Player.MovePlayer(dir, true)
+	}
+	for _, gv := range col.Goals {
+		if col.Player.CheckActivatedGoalCollision(gv, dir) {
+			col.Player.MovePlayer(dir, true)
+		}
+	}
+	col.Player.MovePlayer(dir, false)
 }
